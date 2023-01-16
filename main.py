4dcc775724b611d4a4cb39cb29a79561163d9549
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-16 21:07:27 +0530

    Generating multiple new turtles

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ my_screen.setup(width=500, height=400)
 user_bet = my_screen.textinput(title="Make your bet.", prompt="Which turtle will win?")
 print(user_bet)
 
+colours = ["blue", "red", "green", "yellow", "orange", "purple"]
 
 
-
-
+i=0
+x = -230
+y = -100
+for new_turtle in colours:
+    colours[i] = t(shape="turtle")
+    colours[i].color(new_turtle)
+    colours[i].penup()
+    colours[i].goto(x=x, y=y)
+    i += 1
+    y += 30
 
 
 
