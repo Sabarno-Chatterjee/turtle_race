cc6101b0997ed65b9d9aab0f00eecd7025ae7640
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-17 09:11:27 +0530

    Functional

--- main.py
+++ main.py
@@ -5,18 +5,38 @@ is_race_on = False
 my_screen = Screen()
 my_screen.setup(width=500, height=400)
 user_bet = my_screen.textinput(title="Make your bet.", prompt="Which turtle will win?")
+
+
 # print(user_bet)
+def finish_line():
+    finish = t()
+    finish.hideturtle()
+    finish.pencolor("black")
+    finish.setheading(90)
+    finish.penup()
+    finish.goto(x=210, y=-150)
+    finish.pendown()
+    finish.pensize(10)
+    finish.forward(300)
+    finish.speed(0)
 
-finish = t()
-finish.hideturtle()
-finish.pencolor("black")
-finish.setheading(90)
-finish.penup()
-finish.goto(x=210, y=-170)
-finish.pendown()
-finish.pensize(10)
-finish.forward(340)
 
+starting_spec = 20
+
+
+# def spectators():
+for spectator in range(4):
+    tim = t(shape="turtle")
+    tim.hideturtle()
+    tim.penup()
+    tim.goto(x=starting_spec, y=-180)
+    tim.setheading(90)
+    tim.showturtle()
+    starting_spec += 35
+
+
+
+finish_line()
 colours = ["blue", "red", "green", "yellow", "orange", "purple"]
 all_turtles = []
 i = 0
